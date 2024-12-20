c66015b9ca2aa81c0fc96d0f939011486d16d6d3
김정은 <67693142+Jungeunkim-dev@users.noreply.github.com>
2024-12-20 14:05:46 +0900

    init: springboot 프로젝트 초기화

---
+++ src/main/java/com/flab/jangteo/JangteoApplication.java
@@ -0,0 +1,13 @@
+package com.flab.jangteo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JangteoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JangteoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flab/jangteo/JangteoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.jangteo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JangteoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
