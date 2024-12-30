aa197bf27f74951b238c642c98055ecc2d6bf3b7
김정은 <67693142+Jungeunkim-dev@users.noreply.github.com>
2024-12-31 01:07:12 +0900

    chore: 멀티모듈 구조로 변경

--- jangteo-api/src/test/java/com/flab/jangteoapi/JangteoApiApplicationTests.java
+++ jangteo-api/src/test/java/com/flab/jangteoapi/JangteoApiApplicationTests.java
@@ -1,13 +1,13 @@
-package com.flab.jangteo;
+package com.flab.jangteoapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JangteoApplicationTests {
+class JangteoApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
