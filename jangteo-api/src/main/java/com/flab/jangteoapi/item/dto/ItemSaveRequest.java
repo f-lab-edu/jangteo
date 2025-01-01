f5b3cd7b60879571b10092b47e2e7d561511498b
김정은 <67693142+Jungeunkim-dev@users.noreply.github.com>
2025-01-01 16:05:47 +0900

    fix: Lombok @Builder와 생성자 충돌 문제 해결

--- jangteo-api/src/main/java/com/flab/jangteoapi/item/dto/ItemSaveRequest.java
+++ jangteo-api/src/main/java/com/flab/jangteoapi/item/dto/ItemSaveRequest.java
@@ -2,13 +2,17 @@ package com.flab.jangteoapi.item.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ItemSaveRequest {
 
     private UUID itemId;
