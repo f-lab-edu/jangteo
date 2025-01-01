87999af40c3bca6e899a3394844860f54103d14d
김정은 <67693142+Jungeunkim-dev@users.noreply.github.com>
2025-01-01 16:00:43 +0900

    feat: api dto 검증 목적 의존성 추가, 어노테이션 적용

--- jangteo-api/src/main/java/com/flab/jangteoapi/item/dto/ItemSaveRequest.java
+++ jangteo-api/src/main/java/com/flab/jangteoapi/item/dto/ItemSaveRequest.java
@@ -1,5 +1,7 @@
 package com.flab.jangteoapi.item.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Builder;
 import lombok.Data;
 
@@ -10,11 +12,21 @@ import java.util.UUID;
 public class ItemSaveRequest {
 
     private UUID itemId;
+
+    @NotBlank
     private String itemName;
+
     private String description;
+
+    @NotBlank
     private String category;
+
     private String imageUrl;
+
+    @PositiveOrZero
     private Integer originalPrice;
+
     private String condition;
+
     private String status;
 }
