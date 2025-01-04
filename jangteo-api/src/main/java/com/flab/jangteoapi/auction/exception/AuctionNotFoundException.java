6f3e3cac8d43d552f51fcc5ad31ada55baef165a
김정은 <67693142+Jungeunkim-dev@users.noreply.github.com>
2025-01-04 15:04:14 +0900

    feat: 경매 관리 관련 예외 클래스 추가

---
+++ jangteo-api/src/main/java/com/flab/jangteoapi/auction/exception/AuctionNotFoundException.java
@@ -0,0 +1,8 @@
+package com.flab.jangteoapi.auction.exception;
+
+public class AuctionNotFoundException extends RuntimeException {
+
+    public AuctionNotFoundException(String message) {
+        super(message);
+    }
+}
