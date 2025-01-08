baa45498fad44ae9da2d53fd4b4de092ae05fcd9
김정은 <67693142+Jungeunkim-dev@users.noreply.github.com>
2025-01-09 00:45:49 +0900

    feat: 물품 로직 테스트 목적 레포지토리 코드 작성 (Hashmap 사용)

--- jangteo-api/src/main/java/com/flab/jangteoapi/item/repository/ItemRepository.java
+++ jangteo-api/src/main/java/com/flab/jangteoapi/item/repository/ItemRepository.java
@@ -3,23 +3,36 @@ package com.flab.jangteoapi.item.repository;
 import com.flab.jangteoapi.item.domain.Item;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public class ItemRepository {
 
+    private final Map<UUID, Item> itemDB = new HashMap<>();
+
+
     public Item save(Item item) {
+
+        itemDB.put(item.getItemId(), item);
+
         return item;
     }
 
-    public Item findById(UUID itemId) {
-        return new Item();
+    public Optional<Item> findById(UUID itemId) {
+
+        return Optional.ofNullable(itemDB.get(itemId));
     }
 
     public Boolean existsById(UUID itemId) {
-        return true;
+
+        return itemDB.containsKey(itemId);
     }
 
     public void deleteById(UUID itemId) {
+
+        itemDB.remove(itemId);
     }
 }
